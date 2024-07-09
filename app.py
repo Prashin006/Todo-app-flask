9001dcd70c594d9b43bff82cdf2055176ad58a8b
Prashin Parikh <prashinparikh99@gmail.com>
2024-07-10 00:17:38 +0530

    Complete update and delete features

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -24,11 +24,35 @@ def index():
             db.session.add(new_task)
             db.session.commit()
             return redirect(url_for('index'))
-        except Exception as e:
-            flash(f'An exception occurred!{e}', category='danger')
+        except:
+            return 'An error occurred while creating the task!'
     else:
         tasks = ToDo.query.order_by(ToDo.date_created).all()
-        return render_template('index.html',tasks=tasks)
+        return render_template('index.html',tasks=tasks, db=db)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = ToDo.query.filter_by(id=id).first()
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect(url_for('index'))
+    except:
+        return 'An error occurred while deleting the task!'
+
+@app.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    task_to_update = ToDo.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        task_to_update.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'An error occurred while updating the task!'
+    else:
+        return render_template('update.html',task=task_to_update)
 
 if __name__ == '__main__':
     app.run(debug=True)
