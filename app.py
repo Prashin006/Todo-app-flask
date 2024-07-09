ba7582569dc169b9a782600eada1c6fcac7ee3e1
Prashin Parikh <prashinparikh99@gmail.com>
2024-07-09 15:22:21 +0530

    Create working task management app

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# Specifying /// is a relative path while //// is an absolute path
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///test.db"
+db = SQLAlchemy(app)
 
-@app.route('/')
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return f"<Task {self.id}>"
+
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = ToDo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except Exception as e:
+            flash(f'An exception occurred!{e}', category='danger')
+    else:
+        tasks = ToDo.query.order_by(ToDo.date_created).all()
+        return render_template('index.html',tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
